d1b3ae7caa0d7d1b52000725380c1fbb59e7c190
unknown <AdminSena@MEDDFPREAGOD214.FormacionSena.RED>
2023-03-16 19:43:53 -0500

    dsfads

--- src/main/java/com/microservices/microservices/controller/ControllerVehicle.java
+++ src/main/java/com/microservices/microservices/controller/ControllerVehicle.java
@@ -5,13 +5,14 @@ import com.microservices.microservices.controller.objects.Person;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "https://127.0.0.1:5500")
 public class ControllerVehicle {
     @PostMapping("/sevicio/{dato}")
     public String consultarVehiculo(@PathVariable String dato){
         return "Bienvenido, no hay vehiculos"+dato;
     }
+
     @PostMapping("/services")
-    @ResponseBody
     public Person consultar(){
         Person p = new Person(1,"juan");
         return p;
